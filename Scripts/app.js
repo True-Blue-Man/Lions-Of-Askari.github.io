1ae7f18bc8a274abad6024b899190bae3520bb27
True-Blue-Man <55815658+True-Blue-Man@users.noreply.github.com>
2019-12-20 18:05:57 -0500

    First Commit

---
+++ Scripts/app.js
@@ -0,0 +1,43 @@
+				window.onscroll = function() {
+					myFunction();
+				};
+				var navbar = document.getElementById("navbar");
+				var sticky = navbar.offsetTop;
+
+				function openNav() {
+					document.getElementById("mySidenav").style.width = "250px";
+					document.getElementById("main").style.marginLeft = "250px";
+					document.body.style.backgroundColor = "rgb(0,0,0,0.4)";
+				}
+
+				function closeNav() {
+					document.getElementById("mySidenav").style.width = "0";
+					document.getElementById("main").style.marginLeft = "0";
+					document.body.style.backgroundColor = "white";
+				}
+
+				function myFunction() {
+					if (window.pageYOffset >= sticky) {
+						navbar.classList.add("sticky");
+					} else {
+						navbar.classList.remove("sticky");
+					}
+}
+                
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("linkcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("link");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+  
